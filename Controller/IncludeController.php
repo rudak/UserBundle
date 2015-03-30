4deb7e2e80c37e8efe62be9f172a2a652c0f7cf5
rudak <roiduboul@gmail.com>
2015-03-30 13:38:07 +0200

    delete facebook sdk from branch

--- Controller/IncludeController.php
+++ Controller/IncludeController.php
@@ -2,7 +2,6 @@
 
 namespace Rudak\UserBundle\Controller;
 
-use Rudak\UserBundle\Facebook\FacebookConnect;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,12 +9,7 @@ class IncludeController extends Controller
 {
 	public function userAreaAction()
 	{
-		$facebookConnect = new FacebookConnect('**********', '**********');
-		$connect         = $facebookConnect->connect($this->generateUrl('homepage', array(), true));
-
-		$content = $this->renderView('RudakUserBundle:Include:user-area.html.twig', array(
-			'facebook' => !is_string($connect)
-		));
+		$content = $this->renderView('RudakUserBundle:Include:user-area.html.twig');
 
 		return new Response($content);
 	}
