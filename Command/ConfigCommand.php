31ea88b056dffdb1d61e21914c9c274ac6a7d8a4
rudak <roiduboul@gmail.com>
2015-04-23 09:48:13 +0200

    add command for dump config (rapid config check)

---
+++ Command/ConfigCommand.php
@@ -0,0 +1,35 @@
+<?php
+namespace Rudak\UserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConfigCommand extends ContainerAwareCommand
+{
+
+	protected function configure()
+	{
+		$this
+			->setName('rudakuser:config:dump')
+			->setDescription('Recuperer la configuration du bundle.');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$container = $this->getContainer();
+		$config    = $container->getParameter('rudak.user.config');
+		if (is_array($config)) {
+			$output->writeln("\nConfiguration du RudakUserBundle\n");
+			foreach ($config as $key => $value) {
+				$output->writeln($key . ' : ' . ($value ? $value : 'Default'));
+			}
+		} else {
+			$output->writeln('Erreur de configuration du bundle');
+		}
+
+	}
+
+}
